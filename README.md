335d5ccc3fe6fa2837d2c7d19a2cadb47854ca34
ondrejch <ondrejch@kentico.com>
2020-03-24 13:55:50 +0100

    showcase

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # resolve-url-as-html
+
+## Get started
+
+```sh
+npm install
+npm start
+```
+
+Returns:
+
+```html
+<h3><strong>Logotypes &amp; Badges</strong></h3>
+<p><a data-item-id="2ad895c7-ce0d-4c50-88fa-9461ef6d4774" href="">Kentico Kontent Logotype (zip)</a></p>
+<p><a data-item-id="131339cb-e3d3-4f85-ba0f-319ed11436db" href="">Powered by Kentico Kontent (zip)</a></p>
+<p><a data-item-id="dd146b1e-7f81-4fc1-9a8b-579a83481260" href="">Kentico Kontent Partner Badge (zip)</a></p>
+```
+
+Instead of
+
+```html
+<h3><strong>Logotypes &amp; Badges</strong></h3>
+<p><p>ahoj</p></p>
+<p><p>ahoj</p></p>
+<p><p>ahoj</p></p>
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+const { ContentItem, DeliveryClient, TypeResolver } = require("@kentico/kontent-delivery");
+
+
+  (async () => {
+    class File extends ContentItem {
+      constructor() {
+        super({
+          urlSlugResolver: (link, context) => {
+            return {
+              html: '<p>ahoj</p>'
+            }
+          }
+        });
+      }
+    }
+
+
+    // Tip: Change the project ID of the API call to yours to display your own content. Make sure the codenames still match.
+    const deliveryClient = new DeliveryClient({
+      projectId: '4e9bdd7a-2db8-4c33-a13a-0c368ec2f108',
+      typeResolvers: [
+        new TypeResolver('file', () => new File()),
+      ]
+    });
+
+    // Retrieves the content item from your project
+    const result = await deliveryClient
+      .item('download')
+      .toPromise();
+
+    console.log(result.item.text.resolveData().html);
+  })();
